f4cec20f9b59d0360a4f22a4a5398b9232fa7846
Germán López Pineda <me1lopig@uco.es>
2020-08-22 18:50:21 +0200

    Agregar archivos de proyecto.

---
+++ Genericidad.cpp
@@ -0,0 +1,20 @@
+// Genericidad.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// ejemplo de genericidad en funciones 
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    cout << "Pulsa una tecla para terminar " << endl;
+    cin.get();
+
+    return 0;
+}
+
+
